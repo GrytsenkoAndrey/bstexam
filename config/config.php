539c15642da6595a359d8ae00c96f86ea143c40d
GrytsenkoAndrey <andreyapg@gmail.com>
2020-06-10 16:42:27 +0300

    #1 config

---
+++ config/config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: APG
+ * Date: 10.06.2020
+ * Time: 16:40
+ */
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'myuser');
+define('DB_PASS', 'myuserpass');
+define('DB_NAME', 'uoop');
+define('APPROOT', dirname(dirname(__FILE__)));
+define('URLROOT', 'http://bstexam');
+define('SITENAME', 'BSTi Exam');
