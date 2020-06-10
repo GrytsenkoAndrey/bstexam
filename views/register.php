a75afcc37ed9eef8db5f48a5101cdb2944a00024
GrytsenkoAndrey <andreyapg@gmail.com>
2020-06-10 19:12:09 +0300

    #10 Login page

--- views/register.php
+++ views/register.php
@@ -9,13 +9,13 @@ require APPROOT . '/views/include/header.php';
 
             <form action="<?= URLROOT ?>user/register" method="POST">
                 <div class="form-group">
-                    <label for="email">Email <sup>*</sup></label>
-                    <input type="email" name="email" id="email" class="form-control form-control-lg
+                    <label for="login">Email <sup>*</sup></label>
+                    <input type="email" name="login" id="login" class="form-control form-control-lg
                     <?php
-                    echo (!empty($data['email_error'])) ? ' is-invalid' : '';
+                    echo (!empty($data['login_error'])) ? ' is-invalid' : '';
                     ?>
-                    " value="<?= $data['email'] ?>">
-                    <span class="invalid-feedback"><?= $data['email_error'] ?></span>
+                    " value="<?= $data['login'] ?>">
+                    <span class="invalid-feedback"><?= $data['login_error'] ?></span>
                 </div>
                 <div class="form-group">
                     <label for="password">Пароль <sup>*</sup></label>
